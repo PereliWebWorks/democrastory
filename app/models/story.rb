f6917b953eb4c004641299e33fb10c1e96fab1e7
Andrew Pereli <drewpereli@gmail.com>
2016-11-28 17:34:49 -0600

    added 'parent' method to story model

--- app/models/story.rb
+++ app/models/story.rb
@@ -4,4 +4,15 @@ class Story < ApplicationRecord
   belongs_to :parent, optional: true
   belongs_to :user
   has_many :users, through: :votes
+  #ADD VALIDATION -- If the story is a root story 
+  # it must have a title. And if it's a child story, make sure it's the child of a published story
+  # AND there isn't a published sibling, meaning that no more continuations for the parent are being accepted because
+  # a continuation has already been published
+  def parent
+  	if self.parent_id
+  		Story.find(self.parent_id)
+  	else
+  		nil
+  	end
+  end
 end
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,7 +1,23 @@
 class Vote < ApplicationRecord
   validates :user_id, presence: true
   validates :story_id, presence: true
+  validates_with 
 
   belongs_to :user
   belongs_to :story
+end
+
+
+class VoteValidator < ActiveModel::Validator
+  def validate(record)
+    if record.user.nil? #If the user Id is nil or corresponds to a non-existent user
+    	record.errors[:base] << "Invalid user_id"
+    elseif record.story.nil? #If the story Id is nil or corresponds to a non-existent story
+    	record.errors[:base] << "Invalid story_id"
+    else
+    	#if the vote's story has a published child (meaning voting has closed)
+    	#Or it's a vote on a published story (which should never happen)
+		Story.where("parent_id = ? AND published = true", )
+	end
+  end
 end
