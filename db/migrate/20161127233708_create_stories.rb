93ff969b265cb5ee3abd91fc69fb75dc24c1698e
Andrew Pereli <drewpereli@gmail.com>
2016-12-06 20:03:22 -0600

    attempting deploy again

--- db/migrate/20161127233708_create_stories.rb
+++ db/migrate/20161127233708_create_stories.rb
@@ -3,7 +3,7 @@ class CreateStories < ActiveRecord::Migration[5.0]
     create_table :stories do |t|
       t.string :title
       t.text :contet, null: false
-      t.references :parent, foreign_key: true
+      t.references :story, foreign_key: true
       t.references :user, foreign_key: true, null: false
       t.boolean :published, default: false, null: false
 
