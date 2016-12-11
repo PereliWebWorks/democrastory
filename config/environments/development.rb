f3a5540ac126fdbac896915a2cb84a8ff510c2b9
Andrew Pereli <drewpereli@gmail.com>
2016-12-10 18:50:41 -0600

    set up heroku configuration

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -54,7 +54,8 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :test
-  host = 'warm-basin-82383.herokuapp.com'
+  #host = 'warm-basin-82383.herokuapp.com'
+  host = 'localhost:3000'
   config.action_mailer.default_url_options = { host: host, protocol: 'https' }
 
 end
