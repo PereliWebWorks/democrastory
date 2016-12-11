e0a537e113c48e4a43c64086a2f8fd10f8ecdbf8
Andrew Pereli <drewpereli@gmail.com>
2016-12-10 18:32:39 -0600

    changed host domain to heroku

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -54,7 +54,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :test
-  host = 'localhost:3000'
+  host = 'warm-basin-82383.herokuapp.com'
   config.action_mailer.default_url_options = { host: host, protocol: 'https' }
 
 end
