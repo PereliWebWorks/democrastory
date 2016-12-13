2467d8602f506c40a36887ca3d3b82489233440f
Andrew Pereli <drewpereli@gmail.com>
2016-12-13 16:39:55 -0600

    put vote threshold for publishing into a variable

--- app/models/story.rb
+++ app/models/story.rb
@@ -9,6 +9,10 @@ class Story < ApplicationRecord
   # it must have a title. And if it's a child story, make sure it's the child of a published story
   # AND there isn't a published sibling, meaning that no more continuations for the parent are being accepted because
   # a continuation has already been published
+  def Story.publishThreshhold
+    5
+  end
+
   def parent
   	if self.story
   		self.story
@@ -26,11 +30,11 @@ class Story < ApplicationRecord
   end
 
   def root
-  	rootStory = self #initialize root story with the story being shown
-	while rootStory.parent #While the paret is present, get it and set it to root
-		rootStory = rootStory.story
-	end
-	rootStory
+    rootStory = self #initialize root story with the story being shown
+  	while rootStory.parent #While the paret is present, get it and set it to root
+  		rootStory = rootStory.story
+  	end
+  	rootStory
   end
 
   def isLastPublishedInChain? #If this story has no published children, it's the last story in the chain
@@ -58,7 +62,7 @@ class Story < ApplicationRecord
   end
 
   def publishIfSufficientVotes
-    if self.votes.count >= 10
+    if self.votes.count >= Story.publishThreshhold
       self.publish
     end
   end
